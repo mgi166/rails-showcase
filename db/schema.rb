9eab1d42a106320c591f1ebced9bedd9cb46a0a1
mgi166 <skskoari@gmail.com>
2016-08-13 23:22:43 +0900

    $ rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813120355) do
+ActiveRecord::Schema.define(version: 20160813142145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160813120355) do
     t.string "login",      null: false
     t.string "html_url"
     t.string "avatar_url"
+    t.index ["login"], name: "index_users_on_login", unique: true, using: :btree
   end
 
   add_foreign_key "repositories", "users"
