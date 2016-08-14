c84d316d189de32660f460c5cb84099b6fc66f38
mgi166 <skskoari@gmail.com>
2016-08-14 14:26:05 +0900

    Set default value of User factory.
    * For easily test.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:login) { |n| "username#{n}" }
+    sequence(:html_url) { |n| "https://github.com/username#{n}" }
+    sequence(:avatar_url) { |n| "https://avatars0.githubusercontent.com/u/#{n}" }
   end
 end
