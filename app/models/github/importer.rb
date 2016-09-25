c6bc99659b456bb24676eabf75652cbc5485447a
mgi166 <skskoari@gmail.com>
2016-09-25 18:21:19 +0900

    Add Github::Importer

---
+++ app/models/github/importer.rb
@@ -0,0 +1,45 @@
+module Github
+  class Importer
+    attr_reader :graphql_client
+
+    def initialize
+      @graphql_client = GraphQLClient.new
+    end
+
+    def import_all
+      Github::User.each do |user|
+        Github::RepositoryOwner.each_repos(user.login) do |repo|
+          full_name = "#{user.login}/#{repo.name}"
+
+          repo = Github::Repository.new(
+            full_name,
+            description: repo.description,
+            html_url: repo.homepageURL,
+            forks_count: repo.forks.totalCount,
+            stargazers_count: repo.stargazers.totalCount,
+          )
+
+          if repo.rails?
+            u = create_user(user)
+            create_repo(repo, u)
+          end
+        end
+      end
+    end
+
+    private
+
+    def create_user(user)
+      ::User.find_or_create_by!(login: user.login) do |u|
+        u.avatar_url = user.avatar_url
+        u.html_url = user.html_url
+      end
+    rescue ActiveRecord::RecordNotUnique
+    end
+
+    def create_repo(repo, user)
+      ::Repository.create!(repo.attributes.merge(user: user))
+    rescue ActiveRecord::RecordNotUnique
+    end
+  end
+end
