1964f292c8bb4ee84beed33e2625ad68b522a0bb
mgi166 <skskoari@gmail.com>
2017-03-14 21:59:49 +0900

    Use `constraint` for restriction of url.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'top#index'
 
   resources :repositories, only: [:index]
-  resources :repositories, only: [:show], param: :name
+  resources :repositories, only: [:show], param: :name, constraint: { name: /[^\/]+/ }
   resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
