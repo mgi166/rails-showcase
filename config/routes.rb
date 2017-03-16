2a5593b9fe08421a80b31b8fa50ccfb8cd2c1996
mgi166 <skskoari@gmail.com>
2017-03-16 22:46:46 +0900

    Change users show routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :repositories, only: [:index]
   resources :repositories, only: [:show], param: :name, constraint: { name: /[^\/]+/ }
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index]
+  resources :users, only: [:show], param: :login, constraint: { login: /[^\/]+/ }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
