93ffee9fa2c81b6bf10caa8a30b19c915b9cd82f
mgi166 <skskoari@gmail.com>
2017-03-17 08:05:49 +0900

    Update routing of repository show

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root to: 'top#index'
 
   resources :repositories, only: [:index]
-  resources :repositories, only: [:show], param: :name, constraint: { name: /[^\/]+/ }
   resources :users, only: [:index]
-  resources :users, only: [:show], param: :login, constraint: { login: /[^\/]+/ }
+
+  resources :users, only: [:show], param: :login, constraint: { login: /[^\/]+/ } do
+    resources :repositories, only: [:show], param: :name, constraint: { name: /[^\/]+/ }
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
