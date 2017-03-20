4324b13f99b3d28c7365b1a883f46c4c5fbfd65e
mgi166 <skskoari@gmail.com>
2017-03-20 20:18:21 +0900

    Create like query scope.

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -5,4 +5,11 @@ class Repository < ApplicationRecord
   validates :full_name, presence: true
 
   scope :order_by_stargazers, -> { order('stargazers_count DESC') }
+  scope :with_like_full_name, -> (full_name) { where(arel_table[:full_name].matches("%#{escape_like(full_name)}%")) }
+
+  private
+
+  def self.escape_like(str)
+    str.gsub(/[\\%_]/) { |match| "\\#{match}" }
+  end
 end
