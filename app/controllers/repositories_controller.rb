77f19604ce1d56282e105c15f09e5ade7a93199c
mgi166 <skskoari@gmail.com>
2017-03-20 20:18:33 +0900

    Search with full_name in repository#index.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,7 +3,11 @@ class RepositoriesController < ApplicationController
   before_action :set_repository, only: [:show]
 
   def index
-    @repositories = Repository.order_by_stargazers.includes(:user).page(params[:page])
+    if params[:full_name]
+      @repositories = Repository.with_like_full_name(params[:full_name]).includes(:user).page(params[:page])
+    else
+      @repositories = Repository.order_by_stargazers.includes(:user).page(params[:page])
+    end
   end
 
   def show
