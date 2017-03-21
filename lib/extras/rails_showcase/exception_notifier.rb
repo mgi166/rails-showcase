8b6402a1a4236e9e54685a275cac2d8e6abebfee
mgi166 <skskoari@gmail.com>
2017-03-21 19:59:35 +0900

    Logs to rails log file with exception information

---
+++ lib/extras/rails_showcase/exception_notifier.rb
@@ -0,0 +1,8 @@
+module RailsShowcase
+  module ExceptionNotifier
+    def self.notify(exception)
+      Rails.logger.error "[#{exception.class}] #{exception.message}"
+      Rails.logger.error exception.backtrace.join("\n") if exception.backtrace
+    end
+  end
+end
---
+++ spec/lib/rails_showcase/exception_notifier_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe RailsShowcase::ExceptionNotifier, type: :lib do
+  describe '.notify' do
+    subject { RailsShowcase::ExceptionNotifier.notify(exception) }
+
+    context 'with exception backtrace' do
+      let(:exception) { Exception.new('message') }
+
+      it 'logs to rails log' do
+        expect(Rails.logger).to receive(:error).with("[Exception] message")
+
+        subject
+      end
+    end
+
+    context 'without exception backtrace' do
+      let(:exception) do
+        Exception.new('message').tap do |ex|
+          ex.set_backtrace(['one', 'two', 'three'])
+        end
+      end
+
+      it 'logs to rails log' do
+        expect(Rails.logger).to receive(:error).with("[Exception] message")
+        expect(Rails.logger).to receive(:error).with("one\ntwo\nthree")
+
+        subject
+      end
+    end
+  end
+end
