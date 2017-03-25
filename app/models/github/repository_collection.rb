29d732bf8fceb90559ac5b1caca5ae00920e0b39
mgi166 <skskoari@gmail.com>
2017-03-25 17:28:57 +0900

    Add each_repos method. it's bulk version.

--- app/models/github/repository_collection.rb
+++ app/models/github/repository_collection.rb
@@ -42,5 +42,39 @@ module Github
         next_page = page_info.hasNextPage
       end
     end
+
+    def each_repos
+      return to_enum unless block_given?
+
+      next_page = true
+      after = ''
+
+      while next_page
+        result = graphql_client.repository_owner(login, repository_opts: { after: after })
+
+        # https://github.com/github/graphql-client/blob/ad6b582e9d57b7c5d8203e69be57c1e808351969/lib/graphql/client/response.rb#L13-L33
+        next unless result.respond_to?(:data)
+
+        data = Hashie::Mash.new(result.data.to_h)
+
+        repos = data.repositoryOwner.repositories.edges.map do |edge|
+          node = edge.node
+          Github::Repository.new(
+            "#{login}/#{node.name}",
+            description: node.description,
+            html_url: node.homepageURL,
+            forks_count: node.forks.totalCount,
+            stargazers_count: node.stargazers.totalCount,
+          )
+        end
+
+        yield repos
+
+        page_info = data.repositoryOwner.repositories.pageInfo
+
+        after = page_info.endCursor
+        next_page = page_info.hasNextPage
+      end
+    end
   end
 end
