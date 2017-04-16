e2a287df691dee727b67225c3b1a8ec19961b8fd
mgi166 <skskoari@gmail.com>
2017-04-16 17:08:23 +0900

    Add `order_by` scope

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -5,7 +5,13 @@ class Repository < ApplicationRecord
   validates :full_name, presence: true
 
   scope :order_by_stargazers, -> { order('stargazers_count DESC') }
-  scope :with_like_full_name, -> (full_name) { where(arel_table[:full_name].matches("%#{escape_like(full_name)}%")) }
+  scope :search_with_full_name, -> (full_name) { where(arel_table[:full_name].matches("%#{escape_like(full_name)}%")) }
+  scope :search_order_by, -> (order) {
+    by = ORDER_BY.include?(order.to_s) ? order.to_s : 'id'
+    order(arel_table[by].desc)
+  }
+
+  ORDER_BY = %w(stargazers_count forked_count pushed_at)
 
   def self.search(params)
     if params[:search].present? && params[:repo_or_username].present?
