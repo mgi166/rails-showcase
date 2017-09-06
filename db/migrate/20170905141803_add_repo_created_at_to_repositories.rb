b4266ddeb0f431aff0f81c3ff288de03757e19a0
mgi166 <skskoari@gmail.com>
2017-09-07 08:33:20 +0900

    reop_created_at

--- db/migrate/20170905141803_add_repo_created_at_to_repositories.rb
+++ db/migrate/20170905141803_add_repo_created_at_to_repositories.rb
@@ -1,5 +1,5 @@
 class AddRepoCreatedAtToRepositories < ActiveRecord::Migration[5.1]
   def change
-    add_column :repositories, :repo_created_at, :datetime, null: false
+    add_column :repositories, :repo_created_at, :datetime
   end
 end
