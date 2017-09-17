5eda6e4dcc00e2ced7e85d856c4953f955a36b19
mgi166 <skskoari@gmail.com>
2017-09-18 00:11:06 +0900

    Remove unnecessary methods

--- app/models/github/user.rb
+++ app/models/github/user.rb
@@ -12,24 +12,6 @@ module Github
       def find_by_username(username)
         new.find_by_username(username)
       end
-
-      def find_or_create_by_username!(username)
-        ::User.find_or_create_by!(login: username) do |u|
-          github_user = find_by_username(username)
-          u.avatar_url = github_user.avatar_url
-          u.html_url = github_user.html_url
-        end
-      rescue ActiveRecord::RecordNotUnique
-      end
-
-      def find_or_create_by!(user)
-        ::User.find_or_create_by!(login: user.login) do |u|
-          github_user = find_by_username(user.login)
-          u.avatar_url = github_user.avatar_url
-          u.html_url = github_user.html_url
-        end
-      rescue ActiveRecord::RecordNotUnique
-      end
     end
 
     def initialize
