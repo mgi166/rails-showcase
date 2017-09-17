9b82785a1ecfefff4bcf4f49aa834d4aa8224e4e
mgi166 <skskoari@gmail.com>
2017-09-18 08:02:42 +0900

    Create Github::Importer#import! method
    * Import repositories with idempotency each user.

--- app/models/github/importer.rb
+++ app/models/github/importer.rb
@@ -12,6 +12,39 @@ module Github
       new.import_repos(login)
     end
 
+    def initialize
+      @import_option_for_user = {
+        on_duplicate_key_update: {
+          conflict_target: [:login],
+          columns: Settings.bulk_importer.user_columns
+        },
+        validate: false
+      }
+
+      @import_option_for_repo = {
+        on_duplicate_key_update: {
+          conflict_target: [:name_with_owner],
+          columns: Settings.bulk_importer.repository_columns
+        }
+      }
+    end
+
+    def import!(login)
+      ApplicationRecord.transaction do
+        user = Github::User.find_by_username(login)
+
+        Github::Repository.find_in_batches(user.login) do |repos|
+          results = rails_repos(repos)
+          next if results.blank?
+
+          user_attrs = user.attrs.slice(*Settings.bulk_importer.user_columns.map(&:to_sym))
+          res = ::User.import([user_attrs], @import_option_for_user)
+          results.map! { |r| r.merge(user_id: res.ids.first) }
+          ::Repository.import(results, @import_option_for_repo)
+        end
+      end
+    end
+
     def import_all(since: nil)
       Github::User.each(since: since) do |user|
         Github::Repository.each(user.login) do |repo|
@@ -34,6 +67,13 @@ module Github
       u = Github::User.find_or_create_by!(user)
       repo.create!(u)
     rescue ActiveRecord::RecordNotUnique
+    end
+
+    def rails_repos(repos)
+      Parallel.map(repos, in_threads: 8) do |repo|
+        next unless repo.rails?
+        repo.attributes
+      end.compact
     end
   end
 end
