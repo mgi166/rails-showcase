0236ea3094d8dbdb6250f7919db81f4fb55826a9
mgi166 <skskoari@gmail.com>
2017-09-19 00:21:55 +0900

    Set sidekiq settings with hash

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,13 @@
-redis_connection = proc {
-  Redis.current
-}
-
 Sidekiq.configure_client do |config|
-  config.redis = ConnectionPool.new(size: Settings.sidekiq.client_size, &redis_connection)
+  config.redis = {
+    url: Redis.current.redis.client.options[:url],
+    namespace: Redis.current.namespace,
+  }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = ConnectionPool.new(size: Settings.sidekiq.server_size, &redis_connection)
+  config.redis = {
+    url: Redis.current.redis.client.options[:url],
+    namespace: Redis.current.namespace,
+  }
 end
