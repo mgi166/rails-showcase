3eda937c4b4345644f4c7bf12cf0a29ceead6d40
mgi166 <skskoari@gmail.com>
2017-11-18 16:09:29 +0900

    Select default tab(stargazers_count sorted)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def github_repository_url(repository)
     URI.join('https://github.com', repository.name_with_owner).to_s
   end
+
+  def tab_activated?(key)
+    order = params[:order]
+
+    if order.blank?
+      key == 'stargazers_count'
+    else
+      order == key
+    end
+  end
 end
