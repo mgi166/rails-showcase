9c35d60cfeabf8d037ddf0f322b0064b5549a603
mgi166 <skskoari@gmail.com>
2017-11-18 18:03:22 +0900

    Remove unnecessary util module

--- app/models/concerns/utils/exception_notifier.rb
+++
@@ -1,8 +0,0 @@
-module Utils
-  module ExceptionNotifier
-    def self.notify(exception)
-      Rails.logger.error "[#{exception.class}] #{exception.message}"
-      Rails.logger.error exception.backtrace.join("\n") if exception.backtrace
-    end
-  end
-end
