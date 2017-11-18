971af7bf60ee5bec7a67da0c4398fa1d4554f81e
mgi166 <skskoari@gmail.com>
2017-11-18 15:20:10 +0900

    Use sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
     resources :repositories, only: [:show], param: :name, constraint: { name: /[^\/]+/ }
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
 end
